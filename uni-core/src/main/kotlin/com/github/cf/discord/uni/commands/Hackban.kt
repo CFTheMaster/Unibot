0a86d3845da9dd9f5e2f1d9f867c52d4ef191992
CFTheMaster <hentaimasterpad@gmail.com>
2019-04-30 10:11:53 +0200

    update hackban command

--- uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/Hackban.kt
+++ uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/Hackban.kt
@@ -7,8 +7,6 @@ import com.github.cf.discord.uni.annotations.Perm
 import com.github.cf.discord.uni.entities.Command
 import com.github.cf.discord.uni.entities.Context
 import net.dv8tion.jda.core.Permission
-import net.dv8tion.jda.core.entities.Member
-import net.dv8tion.jda.core.entities.User
 import net.dv8tion.jda.core.exceptions.PermissionException
 
 @Load
@@ -22,17 +20,21 @@ class Hackban : Command(){
     override val guildOnly = true
 
     override fun run(ctx: Context) {
-        ctx.guild!!.controller
-                .ban(ctx.args["user"] as String, 0)
-                .reason("[ ${ctx.author.name}#${ctx.author.discriminator} ] ${ctx.args.getOrDefault("reason", "none")}")
-                .queue({
-                    ctx.send("banned user")
-                }) { err ->
-                    if (err is PermissionException) {
-                        ctx.send("permissions missing can't ban the user")
-                    } else {
-                        ctx.sendError(err)
+        val users = (ctx.args["user"] as String).split("||")
+        for (user in users){
+            ctx.guild!!.controller
+                    .ban(user, 0)
+                    .reason("[ ${ctx.author.name}#${ctx.author.discriminator} ] ${ctx.args.getOrDefault("reason", "none")}")
+                    .queue({
+                        ctx.send("banned user")
+                    }) { err ->
+                        if (err is PermissionException) {
+                            ctx.send("permissions missing can't ban the user")
+                        } else {
+                            ctx.sendError(err)
+                        }
                     }
-                }
+        }
+
     }
 }
