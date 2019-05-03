72374d4818d6e1cc5d369bea1e1d90e58ecbc350
CFTheMaster <hentaimasterpad@gmail.com>
2019-05-03 16:22:46 +0200

    fix some small

--- uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/ViewXP.kt
+++ uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/ViewXP.kt
@@ -19,7 +19,7 @@ import java.time.format.DateTimeFormatter
 
 @Load
 @Argument("user", "user", true)
-class ViewXp : Command(){
+class ViewXP : Command(){
     override val guildOnly = true
     override val desc = "View someone's xp!"
 
