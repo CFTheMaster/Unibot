b8786a11b1a915934ccdb2590365b49d92280c5d
CFTheMaster <hentaimasterpad@gmail.com>
2019-05-22 15:49:53 +0200

    update version number

--- uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/HelpCommand.kt
+++ uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/HelpCommand.kt
@@ -23,7 +23,7 @@ class HelpCommand {
         const val EMBED_TITLE = "Uni Help Page"
         const val WEBSITE_URL = "https://uni.computerfreaker.cf/"
         const val COMMANDS_PER_PAGE = 10
-        const val VERSION_NUMBER = "0.9.7"
+        const val VERSION_NUMBER = "0.9.9"
         @JvmStatic
         val EMBED_COLOUR = Color(125, 165, 222)
     }
