467beaaa0de6c5f00d492bc0e32286e6936c8614
CFTheMaster <hentaimasterpad@gmail.com>
2019-05-22 15:02:35 +0200

    Update Help Command

--- uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/Help.kt
+++ uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/Help.kt
@@ -33,7 +33,7 @@ class Help : Command(){
                     .filter { !it.value.ownerOnly }
                     .toSortedMap()
                     .map {
-                        "\t`${it.key}`: " + " ".repeat(20 - it.key.length) + it.value.desc
+                        "\t`**${it.key}:**` " + " \ndescription:" + it.value.desc + "\n"
                     }
 
             val text = "Flags:\n\n\t-h, --help${" ".repeat(10)}Get help on a command!\n\nCommands:\n\n${commands.joinToString("\n")}"
