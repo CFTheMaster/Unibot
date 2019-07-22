d362c1ccca4b5d9177c8a42e491391223c2b519d
CFTheMaster <hentaimasterpad@gmail.com>
2019-07-22 03:06:23 +0200

    I'm dead iunside

--- uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/Hug.kt
+++ uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/Hug.kt
@@ -19,15 +19,17 @@ import com.github.cf.discord.uni.annotations.Argument
 import com.github.cf.discord.uni.annotations.Load
 import com.github.cf.discord.uni.entities.Command
 import com.github.cf.discord.uni.entities.Context
+import com.github.cf.discord.uni.extensions.findMembers
 import com.github.cf.discord.uni.utils.CFApi
 import net.dv8tion.jda.core.EmbedBuilder
 import net.dv8tion.jda.core.entities.Member
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.jetbrains.kotlin.utils.mapToIndex
 import org.json.JSONObject
 
 @Load
-@Argument("user", "user", true)
+@Argument("user", "string", true)
 class Hug : Command(){
     override val desc = "hug a user"
     override val guildOnly = true
@@ -35,9 +37,12 @@ class Hug : Command(){
     override fun run(ctx: Context) {
         val smolHug = CFApi.getCFApi("hug")
 
+        val mentionedPeople: List<Member> = ctx.msg.mentionedMembers.toList()
         ctx.send(EmbedBuilder().apply {
-            val mem = ctx.args["user"] as Member
-            setTitle(if (ctx.args["user"] == null || ctx.author.id == mem.user.id) "trying to hug yourself " else "${mem.user.name}, you got a hug from ${ctx.member!!.user.name}", smolHug)
+            setTitle(
+                    if (ctx.args == null || ctx.msg.mentionedMembers.first().toString() == ctx.member!!.asMention)  { "trying to hug yourself " }
+                    else
+                        mentionedPeople.asSequence().joinToString { "${it.effectiveName}, "}.removeSurrounding(" ") + " you got a hug from ${ctx.member!!.user.name}" ,smolHug)
             setImage(smolHug)
             setColor(ctx.member?.colorRaw ?: 6684876)
             setFooter("powered by: https://api.computerfreaker.cf", ctx.jda.getUserById(138302166619258880).avatarUrl)
