0d23ba1747df0181b2c77bb9253372ddf83da1b7
CFTheMaster <hentaimasterpad@gmail.com>
2019-09-18 19:09:22 +0200

    oops

--- uni-core/src/main/kotlin/com/github/cf/discord/uni/extensions/Guild.kt
+++ uni-core/src/main/kotlin/com/github/cf/discord/uni/extensions/Guild.kt
@@ -74,7 +74,7 @@ fun Guild.addStar(msg: Message, user: User) {
                 }
 
                 embed.apply {
-                    setAuthor(msg.author.name, null, msg.author.avatarUrl)
+                    setAuthor(msg.author.name + ": click me to go to the message.", msg.jumpUrl, msg.author.avatarUrl)
                     setColor(getStarColor(star[Starboard.stargazers].size + 1))
 
                     if (msg.attachments.isNotEmpty()) {
