5d02fda9c07565beac6238e571b2516463b4f2c3
CFTheMaster <hentaimasterpad@gmail.com>
2020-02-23 15:21:47 +0100

    REEEE

--- uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/ViewXP.kt
+++ uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/ViewXP.kt
@@ -157,8 +157,11 @@ class ViewXP : Command(){
                     val xpNeeded = level.toDouble() * (500).toDouble() + (level.toDouble() * MINIMUM_FOR_LEVEL_1.toDouble())
                     val xpFromLastLevel = (level.toDouble() - 1) * (500).toDouble() + ((level.toDouble() - 1) * MINIMUM_FOR_LEVEL_1.toDouble())
                     val progress = (xp.toDouble() - xpFromLastLevel) / (xpNeeded - xpFromLastLevel) * (100).toDouble()
+                    val usersXP = contract[Users.expPoints] - xpFromLastLevel.toLong()
+                    val xpNeededForLevelUp = xpNeeded - xpFromLastLevel
+                    val progressForNextLevel = progress - xpFromLastLevel
 
-                    processImg(ctx, (contract[Users.expPoints] - xpFromLastLevel.toLong()), (xpNeeded - xpFromLastLevel), progress, level, member,contract[Users.lastLevelUp], contract[Users.accountCreationDate])
+                    processImg(ctx, usersXP, xpNeededForLevelUp, progressForNextLevel, level, member,contract[Users.lastLevelUp], contract[Users.accountCreationDate])
                 }
 
 
