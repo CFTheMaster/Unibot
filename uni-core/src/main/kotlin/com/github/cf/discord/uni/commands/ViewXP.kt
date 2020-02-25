e1e3c2ff6176a800ef2c258863199af1f7853b81
CFTheMaster <hentaimasterpad@gmail.com>
2020-02-25 03:50:28 +0100

    will this work

--- uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/ViewXP.kt
+++ uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/ViewXP.kt
@@ -84,9 +84,6 @@ class ViewXP : Command(){
             g2d.setColor(Color(102, 17, 187, 60))
             g2d.fillRect(0, 0,1500,129)
 
-            g2d.setColor(Color(0,0,0,140))
-            g2d.fillRect((x+200).toInt(), (y+(60*1)+20).toInt(), 800, 30)
-
             g2d.drawImage(profilePicture, 0,  0, null)
 
             g2d.setColor(Color(255,255,255))
@@ -98,9 +95,16 @@ class ViewXP : Command(){
             g2d.setFont(Font(Font.SANS_SERIF, Font.BOLD, 40))
             g2d.drawString("Level: $level", 140, 85)
 
+            val textForExp = "XP For Level: ${userXPPoints}/${xpNeeded.toLong()}, Total: $totalExp"
             g2d.setColor(Color(123,213,139))
             g2d.setFont(Font(Font.SANS_SERIF, Font.BOLD, 40))
-            g2d.drawString("XP For Level: ${userXPPoints}/${xpNeeded.toLong()}, Total: $totalExp", x+200, y+(60*2-10))
+            g2d.drawString(textForExp, x+200, y+(60*2-10))
+            val somethingToWrite = g2d.getFontMetrics(Font.getFont("sans_serif")).stringWidth(textForExp)
+
+
+            g2d.setColor(Color(0,0,0,140))
+            g2d.fillRect((x+200).toInt(), (y+(60*1)+20).toInt(), somethingToWrite, 30)
+
 
 
             g2d.setColor(Color(255,255,255))
