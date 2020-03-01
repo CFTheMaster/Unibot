49f39e4e4da291333902aeceb7b75265e5ee3e86
Emillia <hentaimasterpad@gmail.com>
2020-03-01 13:31:26 +0100

    temp fix
    temp fix till I have everything back up and running on the website again.

--- uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/Invite.kt
+++ uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/Invite.kt
@@ -35,7 +35,7 @@ class Invite : Command(){
         val embed = EmbedBuilder().apply {
             setTitle("invite for bot/support server")
             setColor(embedColor)
-            setDescription("Uni invite: [click me](https://uni.computerfreaker.cf/invite)")
+            setDescription("Uni invite: [click me](https://discordapp.com/oauth2/authorize?client_id=396801832711880715&scope=bot&permissions=-1)")
             addField("invite to my server ", "[server invite](https://discord.gg/DDRbw7W)", true)
             addField("invite to my API server", "[API server invite](https://discord.gg/gzWwtWG)", true)
             setFooter("requested by ${ctx.author.name}#${ctx.author.discriminator} (${ctx.author.id})", "${ctx.author.avatarUrl}")
