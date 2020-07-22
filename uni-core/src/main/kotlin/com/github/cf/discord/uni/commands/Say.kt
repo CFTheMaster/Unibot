08ba349099338194a588c74df6c699be708f79d9
CFTheMaster <hentaimasterpad@gmail.com>
2020-07-22 02:49:38 +0200

    fix say?

--- uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/Say.kt
+++ uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/Say.kt
@@ -32,23 +32,12 @@ class Say : Command(){
     override val cate = Category.OWNER.name
 
     override fun run(ctx: Context) {
-        ctx.send(ctx.args["say"] as String)
-    }
-
-    private fun String.runCommand(workingDir: File): String? {
-        try {
-            val parts = this.split("\\s".toRegex())
-            val proc = ProcessBuilder(*parts.toTypedArray())
-                    .directory(workingDir)
-                    .redirectOutput(ProcessBuilder.Redirect.PIPE)
-                    .redirectError(ProcessBuilder.Redirect.PIPE)
-                    .start()
-
-            proc.waitFor(60, TimeUnit.MINUTES)
-            return proc.inputStream.bufferedReader().readText()
-        } catch(e: IOException) {
-            e.printStackTrace()
-            return null
+        val say = ctx.args["say"] as String
+        if (say.contains("\'")) {
+            say.replace("\'", "\\'")
+        } else if (say.contains("\"")){
+            say.replace("\"", "\\\"")
         }
+        ctx.send(say)
     }
 }
