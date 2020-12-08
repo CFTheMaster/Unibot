1076aa5be46f9690470909c9e72f81f24945e441
Emillia <hentaimasterpad@gmail.com>
2020-12-08 20:43:09 +0100

    give some commands try catch

--- uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/Osu.kt
+++ uni-core/src/main/kotlin/com/github/cf/discord/uni/commands/Osu.kt
@@ -15,6 +15,7 @@
  */
 package com.github.cf.discord.uni.commands
 
+import com.github.cf.discord.uni.Uni
 import com.github.cf.discord.uni.annotations.*
 import com.github.cf.discord.uni.entities.Context
 import com.github.cf.discord.uni.entities.ThreadedCommand
@@ -45,11 +46,21 @@ class Osu : ThreadedCommand() {
 
         val lemmyOsuUrl = "https://osu.computerfreaker.pw/sig.php?colour=hex8866ee&uname=$username&mode=$mode&pp=1&removeavmargin&flagshadow&flagstroke&darkheader&darktriangles&opaqueavatar&avatarrounding=4&rankedscore&onlineindicator=undefined&xpbar&xpbarhex"
 
-        ctx.send(EmbedBuilder().apply {
-            setTitle("User Score For User: $username", null)
-            setImage(lemmyOsuUrl)
-            setColor(ctx.member?.color)
-            setFooter("Image Provided By https://osu.computerfreaker.pw/", null)
-        }.build())
+        try {
+            ctx.send(EmbedBuilder().apply {
+                setTitle("User Score For User: $username", null)
+                setImage(lemmyOsuUrl)
+                setColor(ctx.member?.color)
+                setFooter("Image Provided By https://osu.computerfreaker.pw/", null)
+            }.build())
+        } catch (e: Exception) {
+            ctx.send(EmbedBuilder().apply {
+                setTitle("Command has errored", null)
+                setDescription("Please message: **computerfreaker#0015** and send a screenshot of this error: $e\n**go to the support server: [support server](https://discord.gg/DDRbw7W)**")
+                setFooter("go to support server by doing ${Uni.prefixes.firstOrNull()}support or clicking the link")
+            }.build())
+        }
+
+
     }
 }
